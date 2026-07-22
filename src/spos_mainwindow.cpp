886943cdaf8e094b0e0d6bedd7123d0e9cd625db
PabloDev2195 <chavezpablocesar58@gmail.com>
2026-07-22 15:28:21 -0600

    Main Window Implementation

--- src/spos_mainwindow.cpp
+++ src/spos_mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "spos_mainwindow.h"
 #include "ui_spos_mainwindow.h"
+#include "../src/dialog/dialog_addproduct.h"
+#include "../src/dialog/dialog_modifyproduct.h"
+#include "../src/dialog/dialog_deleteproduct.h"
 
 SPOS_MainWindow::SPOS_MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,4 +14,25 @@ SPOS_MainWindow::SPOS_MainWindow(QWidget *parent)
 SPOS_MainWindow::~SPOS_MainWindow()
 {
     delete ui;
+}
+
+void SPOS_MainWindow::on_action_AddNewProduct_triggered()
+{
+    Dialog_AddProduct dialog(this);
+
+    dialog.exec();
+}
+
+void SPOS_MainWindow::on_action_ModifyProduct_triggered()
+{
+    Dialog_ModifyProduct dialog(this);
+
+    dialog.exec();
+}
+
+void SPOS_MainWindow::on_action_DeleteProduct_triggered()
+{
+    Dialog_DeleteProduct dialog(this);
+
+    dialog.exec();
 }
